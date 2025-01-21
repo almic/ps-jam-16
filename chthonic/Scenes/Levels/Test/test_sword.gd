72588aa95719f7b7c78fc9857727fb8896d1c2e2
TylerStrickland332 <43580115+TylerStrickland332@users.noreply.github.com>
2025-01-21 17:25:09 -0600

    Created Main Menu
    Added fonts for the main menu, created basic non-functional main menu, created start game slicing animation that has yet to be implemented

--- chthonic/Scenes/Levels/Test/test_sword.gd
+++ chthonic/Scenes/Levels/Test/test_sword.gd
@@ -9,20 +9,19 @@ extends Node3D
 @onready var ui_control: Control = %UIControl
 
 func _ready() -> void:
-    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-    GUIDE.enable_mapping_context(combat_context, true)
-    GUIDE.enable_mapping_context(sword_context)
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	GUIDE.enable_mapping_context(combat_context, true)
+	GUIDE.enable_mapping_context(sword_context)
 
 func _process(_delta: float) -> void:
-    if escape.is_triggered():
-        if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-            Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-            ui_control.visible = true
-            GUIDE.disable_mapping_context(combat_context)
-            GUIDE.enable_mapping_context(menu_context)
-        else:
-            Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-            ui_control.visible = false
-            GUIDE.enable_mapping_context(combat_context)
-            GUIDE.disable_mapping_context(menu_context)
-
+	if escape.is_triggered():
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			ui_control.visible = true
+			GUIDE.disable_mapping_context(combat_context)
+			GUIDE.enable_mapping_context(menu_context)
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			ui_control.visible = false
+			GUIDE.enable_mapping_context(combat_context)
+			GUIDE.disable_mapping_context(menu_context)
---
+++ chthonic/UI/Main/ui_main.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $MarginContainer/AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+# Called when start game button is pressed
+func _on_start_game_pressed() -> void:
+	print("you clicked start")
+	animated_sprite_2d.play("SliceAnim")
+	pass # Replace with function body.
